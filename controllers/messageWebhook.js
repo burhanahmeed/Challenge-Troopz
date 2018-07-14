1a8d2d1026bfa96bab1e9ef66f6221ccde9d4fdf
Burhanuddin Ahmad <burhanahmeed@gmail.com>
2018-07-14 08:15:23 +0700

    Update messageWebhook.js

--- controllers/messageWebhook.js
+++ controllers/messageWebhook.js
@@ -16,12 +16,12 @@ exports.index = (req, res) => {
 	console.log(req.body.entry[0].messaging[0])
 	const webhook = req.body.entry[0].messaging[0];
 	var id = webhook.sender.id;
-	if (webhook.message.text === null || webhook.message.text === undefined) {
-	    return true
-	}
-	if (webhook.postback.payload === null || webhook.postback.payload === undefined) {
-	    return true
-	}
+// 	if (webhook.message.text === null || webhook.message.text === undefined) {
+// 	    return true
+// 	}
+// 	if (webhook.postback.payload === null || webhook.postback.payload === undefined) {
+// 	    return true
+// 	}
 	if (webhook.message) {
 		var text = webhook.message.text;
 		console.log('variabel text : '+text)
